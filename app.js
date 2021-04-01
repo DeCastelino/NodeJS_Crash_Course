04e42fd0b0b7fa327a1fb38ecb4205a1529fc841
DeCastelino <s3825140@student.rmit.edu.au>
2021-04-01 15:11:28 +1100

    update in compliance with GCP

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path')
 const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
-// connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://daryll4u:Dar007yll@cluster01.wlh1a.mongodb.net/Project01?retryWrites=true&w=majority";
 
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://daryll4u:Dar007yll@cluster01.wlh1a.mongodb.net/BlogPost?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+	.then(result => app.listen(8080))
+	.catch(err => console.log("Due to database"));
 
 // register view engine
 app.set('view engine', 'ejs');
